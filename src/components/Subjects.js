cc85a8d48cb03ac1663c4cfb6fc910804257ce2f
Stefan Janse van Rensburg <stefanvr250@gmail.com>
2024-10-06 09:31:40 +0200

    added list of subjects to subjects page

--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -1,11 +1,85 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 // component will display all available subjects
 const Subjects = () => {
+    const [subjects, setSubjects] = useState([]);
+
+    useEffect( () => {
+        let subjectList = [
+            {
+                id: 1,
+                title: "Mathematics",
+            },
+            {
+                id: 2,
+                title: "Business Studies",
+            },
+            {
+                id: 3,
+                title: "History",
+            },
+            {
+                id: 4,
+                title: "Geography",
+            },
+            {
+                id: 5,
+                title: "Natural Science",
+            },
+            {
+                id: 6,
+                title: "Life Science",
+            },
+            {
+                id: 7,
+                title: "English",
+            },
+            {
+                id: 8,
+                title: "Technology",
+            },
+            {
+                id: 9,
+                title: "Afrikaans",
+            },
+            {
+                id: 10,
+                title: "Life Skills",
+            },
+            {
+                id: 11,
+                title: "Computer Science",
+            },
+            
+        ];
+
+        setSubjects(subjectList)
+    }, []);
     return(
 
         <div>
             <h2>Subjects</h2>
             <hr />
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Subject</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {subjects.map((m) => (
+                        <tr key={m.id}>
+                            <td>
+                                <Link to= {`/subjects/${m.id}`}>
+                                    {m.title}
+                                </Link>
+                            </td>
+                        </tr>    
+                    ))}
+                </tbody>
+            </table>
+
         </div>
     
     )
